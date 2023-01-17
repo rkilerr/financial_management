920a5dc23eecca3e825a814bd19dfe03847fa6fc
rkilerr <rvjobsupwork@gmail.com>
2023-01-17 12:19:31 -0800

    feat: add validates

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_many :operations
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, presence: true
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,6 @@
 class Operation < ApplicationRecord
   belongs_to :category
+
+  validates :amount, numericality: { only_integer: true, greater_than: 0 }
+  validates :odate, :description, presence: true
 end
